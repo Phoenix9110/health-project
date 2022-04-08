6d8c66a293868dfe32b8e828cb4430beee11c4a3
Phoenix9110 <claus9110@gmail.com>
2022-04-08 11:50:01 -0500

    initial commit nothing special

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+    </Router>jajaja
+  );
+}
+
+export default App;
